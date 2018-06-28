683d55f3408382dcc6aea862d12b397541838f51
Shuprabha Shakya <shshak@microsoft.com>
2018-06-28 13:07:49 -0700

    First Commit of files

---
+++ Main/Sorting/Sorting.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting
+{
+    class Sorting
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sorting");
+            Console.ReadKey();
+        }
+    }
+}
