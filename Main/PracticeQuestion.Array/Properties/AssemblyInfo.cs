204c47d9b457b9fa08d6b9b5f63db1f9c4e0e680
Shuprabha Shakya <shupa643@gmail.com>
2018-07-17 12:06:09 -0700

    Add Array project

--- Main/PracticeQuestion.Array/Properties/AssemblyInfo.cs
+++ Main/PracticeQuestion.Array/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApp1")]
+[assembly: AssemblyTitle("PracticeQuestion.Array")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApp1")]
+[assembly: AssemblyProduct("PracticeQuestion.Array")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b1c4688f-541f-48e0-813f-95a855c5cf1a")]
+[assembly: Guid("1b5e6944-4913-466a-816d-8c5a9cd7b321")]
 
 // Version information for an assembly consists of the following four values:
 //
